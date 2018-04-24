4533c55c3e984ec3ff37aa612f280c12d85fd20d
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-04-23 21:50:00 -0700

    add component state for AddPoll questions and answers form

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class AddPoll extends Component {
+  state = {
+    question: '',
+    a: '',
+    b: '',
+    c: '',
+    d: ''
+  };
   render() {
     return <form className="add-form" />;
   }
