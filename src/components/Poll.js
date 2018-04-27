8ed24d600476f6e6c7869310bbd0c8fb0fd319fa
Frank Ruiz <frank.ruiz.limon@gmail.com>
2018-04-27 12:52:06 -0700

    add comments to Poll mapStateToProps logic

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -20,12 +20,13 @@ function mapStateToProps({ authedUser, polls, users }, { match }) {
             poll: null
         }
     }
-
+    // goal is to reduce vote down to a single value (a, b, c or d)
     const vote = ['aVotes', 'bVotes', 'cVotes', 'dVotes'].reduce((vote, key) => {
-        if (vote != null) {
-            return vote[0];
+        if (vote != null) { // we have already found out which option user has voted for
+            return vote[0]; // vote[0] will return the first letter (a, b, c, d)
         } 
-        
+        // if poll['a/b/c/dVotes'] contains the authedUser, return that key
+        // otherwise return vote (null)
         return poll[key].includes(authedUser) ? key : vote;
     }, null);
 
